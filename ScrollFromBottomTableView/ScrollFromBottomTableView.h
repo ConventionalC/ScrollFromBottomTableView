aa9c1ee0570e99fe9be9e8d083405bfcfa14b107
Peter DeWeese <peter.deweese@gmail.com>
2015-02-01 17:42:59 -0500

    Initial Commit

---
+++ ScrollFromBottomTableView/ScrollFromBottomTableView.h
@@ -0,0 +1,19 @@
+//
+//  ScrollFromBottomTableView.h
+//  ScrollFromBottomTableView
+//
+//  Created by Peter DeWeese on 2/1/15.
+//  Copyright (c) 2015 DeWeese Consulting, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScrollFromBottomTableView.
+FOUNDATION_EXPORT double ScrollFromBottomTableViewVersionNumber;
+
+//! Project version string for ScrollFromBottomTableView.
+FOUNDATION_EXPORT const unsigned char ScrollFromBottomTableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollFromBottomTableView/PublicHeader.h>
+
+
