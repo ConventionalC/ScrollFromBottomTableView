3a31959e2a0f025ca300e1d16ecfe11111aa0fa6
Peter DeWeese <peter.deweese@gmail.com>
2015-02-13 12:14:59 -0500

    Added header imports.

--- ScrollFromBottomTableView/ScrollFromBottomTableView.h
+++ ScrollFromBottomTableView/ScrollFromBottomTableView.h
@@ -1,6 +1,13 @@
 #import <UIKit/UIKit.h>
 #import "ScrollFromBottomTableViewController.h"
 
+FOUNDATION_EXPORT double ScrollFromBottomTableViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char ScrollFromBottomTableViewVersionString[];
+
+#import <ScrollFromBottomTableView/ScrollFromBottomTableViewController.h>
+#import <ScrollFromBottomTableView/ScrollFromBottomTableViewDataSource.h>
+#import <ScrollFromBottomTableView/ScrollFromBottomTableViewDelegate.h>
+
 @interface ScrollFromBottomTableView : UITableView
 
 @end
